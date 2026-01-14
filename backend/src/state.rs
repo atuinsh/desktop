5101c1f914b49bb5e6bdb379a51f2e755d9af00d
Michelle Tilley <michelle@michelletilley.net>
2026-01-14 14:38:04 -0800

    Create new AI database

--- backend/src/state.rs
+++ backend/src/state.rs
@@ -134,6 +134,9 @@ impl AtuinState {
         self.db_instances
             .add_migrator("context", sqlx::migrate!("./migrations/context"))
             .await?;
+        self.db_instances
+            .add_migrator("ai", sqlx::migrate!("./migrations/ai"))
+            .await?;
 
         // For some reason we cannot spawn the exec log task before the state is managed. Annoying.
         let exec_log = ExecLogHandle::new(path).expect("Failed to boot exec log");
